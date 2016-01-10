c3311e451087f44a38fa1915e436cf0f2b0b7bb0
Narendra Sisodiya <narendra@narendrasisodiya.com>
2016-01-10 20:49:55 +0530

    format

--- dist/nodeBlocks.js
+++ dist/nodeBlocks.js
@@ -70,7 +70,7 @@
 
 			'use strict';
 
-			var _Node = __webpack_require__(3);
+			var _Node = __webpack_require__(2);
 
 			var _Node2 = _interopRequireDefault(_Node);
 
@@ -91,108 +91,6 @@
 		},
 		/* 1 */,
 		/* 2 */
-		/***/ function (module, exports) {
-
-			"use strict";
-
-			var _createClass = function () {
-				function defineProperties(target, props) {
-					for (var i = 0; i < props.length; i++) {
-						var descriptor = props[i];
-						descriptor.enumerable = descriptor.enumerable || false;
-						descriptor.configurable = true;
-						if ("value" in descriptor) descriptor.writable = true;
-						Object.defineProperty(target, descriptor.key, descriptor);
-					}
-				}
-
-				return function (Constructor, protoProps, staticProps) {
-					if (protoProps) defineProperties(Constructor.prototype, protoProps);
-					if (staticProps) defineProperties(Constructor, staticProps);
-					return Constructor;
-				};
-			}();
-
-			Object.defineProperty(exports, "__esModule", {
-				value: true
-			});
-
-			function _classCallCheck(instance, Constructor) {
-				if (!(instance instanceof Constructor)) {
-					throw new TypeError("Cannot call a class as a function");
-				}
-			}
-
-			var EventBus = function () {
-				function EventBus() {
-					_classCallCheck(this, EventBus);
-
-					this._topicList = {};
-					this._globalCallbackList = [];
-				}
-
-				_createClass(EventBus, [{
-					key: "subscribe",
-					value: function subscribe(topic, callback) {
-						var _this = this;
-
-						if (typeof topic !== "string" || typeof callback !== "function") {
-							throw "EventBus Unable to subscribe - topic is not string or callback is not a function";
-						}
-						if (this._topicList[topic] === undefined) {
-							this._topicList[topic] = [];
-						}
-
-						var i = this._topicList[topic].push(callback) - 1;
-
-						//UnSub function !!
-						return function () {
-							//Setting Callback as null;
-							_this._topicList[topic][i] = null;
-						};
-					}
-				}, {
-					key: "subscribeAll",
-					value: function subscribeAll(callback) {
-						var _this2 = this;
-
-						var i = this._globalCallbackList.push(callback) - 1;
-						return function () {
-							_this2._globalCallbackList[i] = null;
-						};
-					}
-				}, {
-					key: "publish",
-					value: function publish(topic) {
-						for (var _len = arguments.length, args = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
-							args[_key - 1] = arguments[_key];
-						}
-
-						if (this._topicList[topic] !== undefined) {
-							this._topicList[topic].map(function (callback) {
-								if (callback !== null) {
-									//SKIP the unsubscribed callback !
-									callback.apply(null, args);
-								}
-							});
-						}
-						this._globalCallbackList.map(function (callback) {
-							if (callback !== null) {
-								//SKIP the unsubscribed callback !
-								callback.apply(null, [topic].concat(args));
-							}
-						});
-					}
-				}]);
-
-				return EventBus;
-			}();
-
-			exports.default = EventBus;
-
-			/***/
-		},
-		/* 3 */
 		/***/ function (module, exports, __webpack_require__) {
 
 			'use strict';
@@ -227,7 +125,7 @@
 				value: true
 			});
 
-			var _EventBus = __webpack_require__(2);
+			var _EventBus = __webpack_require__(3);
 
 			var _EventBus2 = _interopRequireDefault(_EventBus);
 
@@ -296,6 +194,108 @@
 
 			/***/
 		},
+		/* 3 */
+		/***/ function (module, exports) {
+
+			"use strict";
+
+			var _createClass = function () {
+				function defineProperties(target, props) {
+					for (var i = 0; i < props.length; i++) {
+						var descriptor = props[i];
+						descriptor.enumerable = descriptor.enumerable || false;
+						descriptor.configurable = true;
+						if ("value" in descriptor) descriptor.writable = true;
+						Object.defineProperty(target, descriptor.key, descriptor);
+					}
+				}
+
+				return function (Constructor, protoProps, staticProps) {
+					if (protoProps) defineProperties(Constructor.prototype, protoProps);
+					if (staticProps) defineProperties(Constructor, staticProps);
+					return Constructor;
+				};
+			}();
+
+			Object.defineProperty(exports, "__esModule", {
+				value: true
+			});
+
+			function _classCallCheck(instance, Constructor) {
+				if (!(instance instanceof Constructor)) {
+					throw new TypeError("Cannot call a class as a function");
+				}
+			}
+
+			var EventBus = function () {
+				function EventBus() {
+					_classCallCheck(this, EventBus);
+
+					this._topicList = {};
+					this._globalCallbackList = [];
+				}
+
+				_createClass(EventBus, [{
+					key: "subscribe",
+					value: function subscribe(topic, callback) {
+						var _this = this;
+
+						if (typeof topic !== "string" || typeof callback !== "function") {
+							throw "EventBus Unable to subscribe - topic is not string or callback is not a function";
+				}
+						if (this._topicList[topic] === undefined) {
+							this._topicList[topic] = [];
+						}
+
+						var i = this._topicList[topic].push(callback) - 1;
+
+						//UnSub function !!
+						return function () {
+							//Setting Callback as null;
+							_this._topicList[topic][i] = null;
+						};
+					}
+				}, {
+					key: "subscribeAll",
+					value: function subscribeAll(callback) {
+						var _this2 = this;
+
+						var i = this._globalCallbackList.push(callback) - 1;
+						return function () {
+							_this2._globalCallbackList[i] = null;
+						};
+					}
+				}, {
+					key: "publish",
+					value: function publish(topic) {
+						for (var _len = arguments.length, args = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+							args[_key - 1] = arguments[_key];
+				}
+
+						if (this._topicList[topic] !== undefined) {
+							this._topicList[topic].map(function (callback) {
+								if (callback !== null) {
+									//SKIP the unsubscribed callback !
+									callback.apply(null, args);
+								}
+							});
+						}
+						this._globalCallbackList.map(function (callback) {
+							if (callback !== null) {
+								//SKIP the unsubscribed callback !
+								callback.apply(null, [topic].concat(args));
+					}
+						});
+					}
+				}]);
+
+				return EventBus;
+			}();
+
+			exports.default = EventBus;
+
+			/***/
+		},
 		/* 4 */
 		/***/ function (module, exports, __webpack_require__) {
 
@@ -326,7 +326,7 @@
 				value: true
 			});
 
-			var _Node = __webpack_require__(3);
+			var _Node = __webpack_require__(2);
 
 			var _Node2 = _interopRequireDefault(_Node);
 
@@ -355,7 +355,7 @@
 						var _this = this;
 
 						this.in = {};
-						this.getConfig().in.map(function (v) {
+						this.getConfig1().in.map(function (v) {
 							_this.in[v] = new _Node2.default();
 							_this.in[v].listen(function () {
 								_this.run(_this.in, _this.out);
